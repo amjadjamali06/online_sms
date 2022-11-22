f70064aa564fc6914372a9d6b06c28e4f1b47253
AMJAD-ALI <amjadjamali06@gmail.com>
2022-11-22 18:12:04 +0500

    Implemented Sim Info Packages

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/services.dart';
+import 'package:mobile_number/mobile_number.dart';
+import 'package:sim_data/sim_data.dart' as sim;
+
 import '../app_theme.dart';
 import 'package:flutter/material.dart';
 import '../widgets/widgets.dart';
@@ -29,6 +33,35 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     super.initState();
   }
 
+  Future<void> initMobileNumberState() async {
+    if (!await MobileNumber.hasPhonePermission) {
+      await MobileNumber.requestPhonePermission;
+      return;
+    }
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      String _mobileNumber = (await MobileNumber.mobileNumber)!;
+      var _simCard = (await MobileNumber.getSimCards)!;
+
+      print('-----Number> $_mobileNumber');
+      for(SimCard s in _simCard) {
+
+        print('==========> ${s.toMap()}');
+
+      }
+    } on PlatformException catch (e) {
+      debugPrint("Failed to get mobile number because of '${e.message}'");
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    // setState(() {});
+  }
+
+
   @override
   void dispose() {
     tabController.addListener(() {
@@ -42,12 +75,35 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+
+    initMobileNumberState();
+    try {
+      sim.SimDataPlugin.getSimData().then((simData) {
+        for (var s in simData.cards) {
+          print('------------1> ${s.carrierName}');
+          print('------------2> ${s.countryCode}');
+          print('------------3> ${s.displayName}');
+          print('------------4> ${s.isDataRoaming}');
+          print('------------5> ${s.isNetworkRoaming}');
+          print('------------6> ${s.mcc}');
+          print('------------7> ${s.mnc}');
+          print('------------8> ${s.serialNumber}');
+          print('------------9> ${s.slotIndex}');
+          print('------------0> ${s.subscriptionId}');
+        }
+      });
+
+    } on PlatformException catch (e) {
+      debugPrint("error! code: ${e.code} - message: ${e.message}");
+    }
+
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: MyTheme.kPrimaryColor,
         leading: IconButton(
           onPressed: () {},
-          icon: Icon(Icons.menu),
+          icon: const Icon(Icons.menu),
         ),
         title: Text(
           'Chattie',
@@ -55,7 +111,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.camera_alt),
+            icon: const Icon(Icons.camera_alt),
             onPressed: () {},
           )
         ],
@@ -67,8 +123,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           MyTabBar(tabController: tabController),
           Expanded(
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(30),
@@ -76,7 +132,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                   )),
               child: TabBarView(
                 controller: tabController,
-                children: [
+                children: const [
                   ChatPage(),
                   Center(child: Text('Status')),
                   Center(child: Text('Call')),
